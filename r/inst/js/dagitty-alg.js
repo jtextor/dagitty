4b922626749ad1512c4aba0e7e362466c0e71dd9
Benito van der Zander <benito@benibela.de>
2016-09-23 14:19:06 +0200

    do not create so many nodes of one kind that it cannot satisfy the min limit of other kinds of nodes. 2nd attempt

--- r/inst/js/dagitty-alg.js
+++ r/inst/js/dagitty-alg.js
@@ -3949,18 +3949,19 @@ var GraphGenerator = {
 	 * parameters for the setRandomNodes function (see below)
 	 */
 	randomDAG : function( variables, p){
-		var g, i, j
+		var i, j
 		if (typeof variables == "number" ) {
 			var n = variables
 			variables = []
 			for (i = 1; i <= n; i++) variables.push("v" + i)
 		}
-		g = new Graph()
+		var g = new Graph()
 		var vertices = [] 
 		for( i = 0 ; i < variables.length ; i ++ ){
 			var v = g.addVertex( variables[i] )
 			vertices.push(v)
-		}		
+		}
+		
 		var pEdge = p
 		if (typeof p == "object") {
 			pEdge = p.pEdge
@@ -3985,34 +3986,47 @@ var GraphGenerator = {
 	 * pTarget, minTarget, maxTarget, pLatentNode, minLatentNode, maxLatentNode control the creation of other node types.
 	*/
 	setRandomNodes: function (g, p) {
+		var i, j, k
 		var vertices = g.vertices.values()
 		var prop = ["Source", "Target", "LatentNode"]
-		var i
 		for (i=0;i<prop.length;i++) g["removeAll"+prop[i]+"s"]()
+		for (i=0,j=1,k=2;i<prop.length;i++,j++,k++) {
+			if (j >= prop.length) j = 0
+			if (k >= prop.length) k = 0
+			var minOthers = (p["min"+prop[j]] ? p["min"+prop[j]] : 0) + (p["min"+prop[k]] ? p["min"+prop[k]] : 0)
+			if (!(("max" + prop[i])  in p) || (p["max"+prop[i]] > vertices.length - minOthers ))
+				p["max"+prop[i]] = vertices.length - minOthers
+		}
 		var pSource = p.pSource ? p.pSource : 0, 
 			pTarget = p.pTarget ? p.pTarget : 0, 
 			pLatentNode = p.pLatentNode ? p.pLatentNode : 0
-		var maxSource = p.maxSource ? p.maxSource : vertices.length,
-			maxTarget = p.maxTarget ? p.maxTarget : vertices.length,
-			maxLatentNode = p.maxLatentNode ? p.maxLatentNode : vertices.length
+		var maxSource = "maxSource" in p ? p.maxSource : vertices.length,
+			maxTarget = "maxTarget" in p ? p.maxTarget : vertices.length,
+			maxLatentNode = "maxLatentNode" in p ? p.maxLatentNode : vertices.length
+		
 		var counts = {"Source": 0, "Target": 0, "LatentNode": 0}
 		var availableVertices = []
 		for (i=0;i<vertices.length;i++) {
 			var v = vertices[i]
 			var q = Math.random()
-			if (q < pSource) { if (counts.Source < maxSource) { g.addSource(v); counts.Source++ } }
-			else if (q < pSource + pTarget) { if (counts.Target < maxTarget) { g.addTarget(v); counts.Target++ } }
-			else if (q < pSource + pTarget + pLatentNode) { if (counts.LatentNode < maxLatentNode) { g.addLatentNode(v);  counts.LatentNode++ } }
-			else availableVertices.push(v)
-		}	  
+			var kind = null
+			if (q < pSource) { if (counts.Source < maxSource) kind = "Source" }
+			else if (q < pSource + pTarget) { if (counts.Target < maxTarget) kind = "Target" }
+			else if (q < pSource + pTarget + pLatentNode) { if (counts.LatentNode < maxLatentNode) kind = "LatentNode" }
+			if (kind)	{
+				g["add" + kind](v) 
+				counts[kind]++ 
+			} else availableVertices.push(v)
+		}
 		for (i=0;i<prop.length;i++) {
 			if (!p["min"+prop[i]]) continue
 			for (var existing = counts[prop[i]]; existing < p["min"+prop[i]]; existing++) {
-				var j = Math.floor(Math.random() * availableVertices.length)
+				if (availableVertices.length == 0) throw "no available vertices"
+				j = Math.floor(Math.random() * availableVertices.length)
 				g["add"+prop[i]](availableVertices[j])
 				availableVertices.splice(j, 1)
 			}
-		}
+		} 
 	}
 }
 /* DAGitty - a browser-based software for causal modelling and analysis
