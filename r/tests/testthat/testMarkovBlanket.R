79b1a78c8ab184a464b32689dea91d2a0b58fdfc
Johannes Textor <j.textor@science.ru.nl>
2022-03-04 11:36:30 -0800

    further tests Markov blanket

--- r/tests/testthat/testMarkovBlanket.R
+++ r/tests/testthat/testMarkovBlanket.R
@@ -10,8 +10,11 @@ fig_2.9 <- dagitty(
   )
 
 test_that("Markov Blanket", {
-	expect_equal( sort( markovBlanket(fig_2.9, "Z_1") ) , c("X", "Z_2", "Z_3") )
-	expect_equal( sort( markovBlanket(fig_2.9, "Y") ), c("W", "Z_2", "Z_3") )
+	expect_equal( sort( markovBlanket( fig_2.9, c() ) ), character(0) )
+	expect_equal( sort( markovBlanket( fig_2.9, list() ) ), character(0) )
+	expect_equal( sort( markovBlanket( fig_2.9, "Z_1" ) ), c("X", "Z_2", "Z_3") )
+	expect_equal( sort( markovBlanket( fig_2.9, "Y" ) ), c("W", "Z_2", "Z_3") )
+	expect_equal( sort( markovBlanket( fig_2.9, c("W","Y") ) ), c("X", "Z_2", "Z_3") )
 } )
 
 
