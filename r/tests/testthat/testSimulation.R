e025d1d2f2e4389adb2bd0a57082bc42ab228da2
Johannes Textor <johannes.textor@ru.nl>
2023-12-07 12:04:56 +0100

    fixed random DAG generation

--- r/tests/testthat/testSimulation.R
+++ r/tests/testthat/testSimulation.R
@@ -1,3 +1,10 @@
 test_that("simulation methods", {
 	expect_equal( nrow( simulateLogistic( "dag{X}", N=1000 ) ), 1000 )
 })
+
+test_that("random graph generation", {
+	expect_equal( length( names( randomDAG( 10, .5 ) ) ), 10 )
+	expect_equal( nrow( edges( randomDAG( 5, 0 ) ) ) , 0 )
+	expect_equal( nrow( edges( randomDAG( 3, 1 ) ) ), 3 )
+
+} )
